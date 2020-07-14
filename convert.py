23fafb6eae07e242569f17b44bec57f2429ad574
myOmikron <accounts@omikron.pw>
2020-07-14 22:09:51 +0200

    ffmpeg and convert.py added

---
+++ convert.py
@@ -0,0 +1,232 @@
+from xml.dom import minidom
+import sys
+import os
+import shutil
+import ffmpeg
+import re
+import time
+
+tmp = sys.argv[1].split('-')
+try:
+    if tmp[2] == 'presentation':
+        meetingId = tmp[0] + '-' + tmp[1]
+    else:
+        sys.exit()
+except IndexError:
+    meetingId = sys.argv[1]
+
+PATH = '/var/bigbluebutton/published/presentation/'
+source_dir = PATH + meetingId + "/"
+temp_dir = source_dir + 'temp/'
+target_dir = source_dir + 'download/'
+events_file = 'shapes.svg'
+source_events = '/var/bigbluebutton/recording/raw/' + meetingId + '/events.xml'
+# Deskshare
+SOURCE_DESKSHARE = source_dir + 'deskshare/deskshare.webm'
+TMP_DESKSHARE_FILE = temp_dir + 'deskshare.mp4'
+
+
+def extract_timings(bbb_version):
+    doc = minidom.parse(events_file)
+    dictionary = {}
+    total_length = 0
+    j = 0
+
+    for image in doc.getElementsByTagName('image'):
+        path = image.getAttribute('xlink:href')
+
+        if j == 0 and '2.0.0' > bbb_version:
+            path = u'/usr/local/bigbluebutton/core/scripts/logo.png'
+            j += 1
+
+        in_times = str(image.getAttribute('in')).split(' ')
+        out_times = image.getAttribute('out').split(' ')
+
+        temp = float(out_times[len(out_times) - 1])
+        if temp > total_length:
+            total_length = temp
+
+        occurrences = len(in_times)
+        for i in range(occurrences):
+            dictionary[float(in_times[i])] = temp_dir + str(path)
+
+    return dictionary, total_length
+
+
+def create_slideshow(dictionary, length, result):
+    video_list = 'video_list.txt'
+    video_list_tmp = []
+    video_list_fn = []
+    f = open(video_list, 'w')
+
+    times = dictionary.keys()
+    times.sort()
+
+    ffmpeg.webm_to_mp4(SOURCE_DESKSHARE, TMP_DESKSHARE_FILE)
+
+    print("-=create_slideshow=-")
+    for i, t in enumerate(times):
+        tmp_name = '%d.mp4' % i
+        tmp_ts_name = '%d.ts' % i
+        image = dictionary[t]
+
+        if i == len(times) - 1:
+            duration = length - t
+        else:
+            duration = times[i + 1] - t
+
+        out_file = temp_dir + tmp_name
+        out_ts_file = temp_dir + tmp_ts_name
+
+        if "deskshare.png" in image:
+            print(sys.stderr, (0, i, t, duration))
+            ffmpeg.trim_video_by_seconds(TMP_DESKSHARE_FILE, t, duration, out_file)
+            ffmpeg.mp4_to_ts(out_file, out_ts_file)
+        else:
+            print(sys.stderr, (1, i, t, duration))
+            ffmpeg.create_video_from_image(image, duration, out_ts_file)
+        video_list_fn.append(out_ts_file)
+        f.write('file ' + out_ts_file + '\n')
+        video_list_tmp.append(out_ts_file)
+    f.close()
+    height = 1080
+    # Scale video files
+    with open(video_list, 'w') as f:
+        for entry in video_list_fn:
+            out_file = temp_dir + entry.split('/')[-1].split('.')[0] + '_new.ts'
+            ffmpeg.scale_fixed(entry, out_file)
+            f.write('file ' + out_file + '\n')
+    ffmpeg.concat_videos(video_list, result)
+    return height
+
+
+def get_presentation_dims(presentation_name):
+    doc = minidom.parse(events_file)
+    images = doc.getElementsByTagName('image')
+
+    for el in images:
+        name = el.getAttribute('xlink:href')
+        pattern = presentation_name
+        if re.search(pattern, name):
+            height = int(el.getAttribute('height'))
+            width = int(el.getAttribute('width'))
+            return height, width
+
+
+def rescale_presentation(new_height, new_width, dictionary, bbb_version):
+    times = dictionary.keys()
+    times.sort()
+    for i, t in enumerate(times):
+        if i < 1 and '2.0.0' > bbb_version:
+            continue
+        ffmpeg.rescale_image(dictionary[t], new_height, new_width, dictionary[t])
+
+
+def check_presentation_dims(dictionary, dims, bbb_version):
+    names = dims.keys()
+    heights = []
+    widths = []
+
+    for i in names:
+        temp = dims[i]
+        heights.append(temp[0])
+        widths.append(temp[1])
+
+    height = max(heights)
+    width = max(widths)
+
+    print("MAX Height:" + str(height))
+
+    dim1 = height % 2
+    dim2 = width % 2
+
+    new_height = height
+    new_width = width
+
+    if dim1 or dim2:
+        if dim1:
+            new_height += 1
+        if dim2:
+            new_width += 1
+
+    rescale_presentation(new_height, new_width, dictionary, bbb_version)
+
+
+def prepare(bbb_version):
+    if not os.path.exists(target_dir):
+        os.mkdir(target_dir)
+
+    if not os.path.exists(temp_dir):
+        os.mkdir(temp_dir)
+
+    shutil.copytree("presentation", temp_dir + "presentation")
+    dictionary, length = extract_timings(bbb_version)
+    print("dictionary")
+    print(dictionary)
+    print("length")
+    print(length)
+    dims = get_different_presentations(dictionary)
+    print("dims")
+    print(dims)
+    check_presentation_dims(dictionary, dims, bbb_version)
+    return dictionary, length, dims
+
+
+def get_different_presentations(dictionary):
+    times = dictionary.keys()
+    print("times")
+    print(times)
+    presentations = []
+    dims = {}
+    for t in times:
+        name = dictionary[t].split("/")[7]
+        print("name")
+        print(name)
+        if name not in presentations:
+            presentations.append(name)
+            dims[name] = get_presentation_dims(name)
+
+    return dims
+
+
+def cleanup():
+    if os.path.exists(temp_dir):
+        shutil.rmtree(temp_dir)
+
+    if os.path.exists(target_dir):
+        shutil.rmtree(target_dir)
+
+
+def bbbversion():
+    bbb_ver = 0
+    s_events = minidom.parse(source_events)
+    for event in s_events.getElementsByTagName('recording'):
+        bbb_ver = event.getAttribute('bbb_version')
+    return bbb_ver
+
+
+def main():
+    print("\n<-------------------" + time.strftime("%c") + "----------------------->\n")
+
+    bbb_version = bbbversion()
+    print("bbb_version: " + bbb_version)
+
+    os.chdir(source_dir)
+    dictionary, length, dims = prepare(bbb_version)
+
+    slideshow = source_dir + 'slideshow.mp4'
+
+    try:
+        height = create_slideshow(dictionary, length, slideshow)
+        if os.path.isfile(source_dir + 'video/webcams.webm') and not os.path.isfile(source_dir + 'video/webcams.mp4'):
+            ffmpeg.make_mp4(source_dir + 'video/webcams.webm', source_dir + 'video/webcams.mp4')
+        ffmpeg.scale_height(height, source_dir + 'video/webcams.mp4', source_dir + 'video/webcams_new.mp4')
+        ffmpeg.concat_presentation_webcam(slideshow, source_dir + 'video/webcams_new.mp4', source_dir + 'output.mp4')
+    finally:
+        print(sys.stderr, "Cleaning up temp files...")
+        cleanup()
+        print(sys.stderr, "Done")
+
+
+if __name__ == "__main__":
+    main()
---
+++ ffmpeg.py
@@ -0,0 +1,145 @@
+# Python wrapper around the ffmpeg utility
+import os
+import shutil
+
+# FFMPEG = 'ffmpeg'
+FFMPEG = '/opt/ffmpeg/ffmpeg'
+# FFMPEG = '/root/bin/ffmpeg'
+VID_ENCODER = 'libx264'
+
+
+def set_logfile(file):
+    global logfile
+    logfile = file
+
+def make_mp4(video_file, out_file):
+    command = '%s -i %s -c:v libx264 %s -y' % (
+        FFMPEG, video_file, out_file)
+    os.system(command)
+
+def scale_fixed(video_file, out_file):
+    command = '%s -i %s -vf "scale=1920:1080:force_original_aspect_ratio=decrease,pad=1920:1080:(ow-iw)/2:(oh-ih)/2,setsar=1" -c:v libx264 -max_muxing_queue_size 1024  %s -y' % (
+        FFMPEG, video_file, out_file)
+    print("Calling scale_fixed" + command)
+    os.system(command)
+
+def scale_height(height, video_file, out_file):
+    command = '%s -i %s -vf scale=-2:%s %s -y' % (
+         FFMPEG, video_file, str(height), out_file)
+    print("Calling scale_height" + command)
+    os.system(command)
+
+def concat_presentation_webcam(presentation_file, webcam_file, out_file):
+    command = '%s -i %s -i %s -max_muxing_queue_size 1024 -c:v libx264 -filter_complex hstack -r 24 %s -y' % (
+        FFMPEG, presentation_file, webcam_file, out_file)
+    os.system(command)
+
+def mux_slideshow_audio(video_file, audio_file, out_file):
+    command = '%s -i %s -i %s -map 0 -map 1 -codec copy -shortest %s 2>> %s' % (
+        FFMPEG, video_file, audio_file, out_file, logfile)
+    print("Calling mux_slideshow_audio:" + command)
+    os.system(command)
+
+
+def extract_audio_from_video(video_file, out_file):
+    command = '%s -i %s -ab 160k -ac 2 -ar 44100 -vn %s 2>> %s' % (FFMPEG, video_file, out_file, logfile)
+    print("Calling extract_audio_from_video:" + command)
+    os.system(command)
+
+
+def create_video_from_image(image, duration, out_file):
+    print("*************** create_video_from_image ******************")
+    print(image, "\n", duration, "\n", out_file)
+    command = '%s -loop 1 -r 5 -f image2 -i %s -c:v %s -t %s -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" %s 2>> %s' % (
+        FFMPEG, image, VID_ENCODER, duration, out_file, logfile)
+    print("Calling create_video_from_image" + command)
+    os.system(command)
+
+
+def concat_videos(video_list, out_file):
+    command = '%s -y -f concat -safe 0 -i %s -c copy %s 2>> %s' % (FFMPEG, video_list, out_file, logfile)
+    print("Calling concat_videos:" + command)
+    os.system(command)
+
+
+def mp4_to_ts(input, output):
+    command = '%s -i %s -c copy -bsf:v h264_mp4toannexb -f mpegts %s 2>> %s' % (FFMPEG, input, output, logfile)
+    print("Calling mp4_to_ts:" + command)
+    os.system(command)
+
+
+def concat_ts_videos(input, output):
+    command = '%s -i %s -c copy -bsf:a aac_adtstoasc %s 2>> %s' % (FFMPEG, input, output, logfile)
+    print("Calling concat_ts_videos:" + command)
+    os.system(command)
+
+
+def rescale_image(image, height, width, out_file):
+    if height < width:
+        command = '%s -i %s -vf pad=%s:%s:0:oh/2-ih/2 %s -y 2>> %s' % (FFMPEG, image, width, height, out_file, logfile)
+    else:
+        command = '%s -i %s -vf pad=%s:%s:0:ow/2-iw/2 %s -y 2>> %s' % (FFMPEG, image, width, height, out_file, logfile)
+
+    os.system(command)
+
+
+def trim_video(video_file, start, end, out_file):
+    start_h = start / 3600
+    start_m = start / 60 - start_h * 60
+    start_s = start % 60
+
+    end_h = end / 3600
+    end_m = end / 60 - end_h * 60
+    end_s = end % 60
+
+    str1 = '%d:%d:%d' % (start_h, start_m, start_s)
+    str2 = '%d:%d:%d' % (end_h, end_m, end_s)
+    command = '%s -ss %s -t %s -i %s -vcodec copy -acodec copy %s 2>> %s' % (
+    FFMPEG, str1, str2, video_file, out_file, logfile)
+    os.system(command)
+
+
+def trim_video_by_seconds(video_file, start, end, out_file):
+    command = '%s -ss %s -i %s -c copy -t %s %s 2>> %s' % (FFMPEG, start, video_file, end, out_file, logfile)
+    os.system(command)
+
+
+def trim_audio(audio_file, start, end, out_file):
+    temp_file = 'temp.mp3'
+    start_h = start / 3600
+    start_m = start / 60 - start_h * 60
+    start_s = start % 60
+
+    end_h = end / 3600
+    end_m = end / 60 - end_h * 60
+    end_s = end % 60
+
+    str1 = '%d:%d:%d' % (start_h, start_m, start_s)
+    str2 = '%d:%d:%d' % (end_h, end_m, end_s)
+    command = '%s -ss %s -t %s -i %s %s >> %s' % (FFMPEG, str1, str2, audio_file, temp_file, logfile)
+    print(command)
+    os.system(command)
+    mp3_to_aac(temp_file, out_file)
+    os.remove(temp_file)
+
+
+def trim_audio_start(dictionary, length, full_audio, audio_trimmed):
+    times = dictionary.keys()
+    times.sort()
+    trim_audio(full_audio, int(round(times[0])), int(length), audio_trimmed)
+
+
+def trim_video_start(dictionary, length, full_vid, video_trimmed):
+    times = dictionary.keys()
+    times.sort()
+    trim_video(full_vid, int(round(times[2])), int(length), video_trimmed)
+
+
+def mp3_to_aac(mp3_file, aac_file):
+    command = '%s -i %s -c:a libfdk_aac %s 2>> %s' % (FFMPEG, mp3_file, aac_file, logfile)
+    os.system(command)
+
+
+def webm_to_mp4(webm_file, mp4_file):
+    command = '%s -i %s -qscale 0 %s 2>> %s' % (FFMPEG, webm_file, mp4_file, logfile)
+    os.system(command)
